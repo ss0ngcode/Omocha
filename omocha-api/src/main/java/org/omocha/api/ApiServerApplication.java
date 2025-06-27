d74b95eb73c75f80469f57d62a31e0ffc63b08c9
song <shd2768@naver.com>
2025-06-27 10:38:35 +0900

    refactor: @EnableRetry 누락 추가
    - Spring Retry 의존성 전파

--- omocha-api/src/main/java/org/omocha/api/ApiServerApplication.java
+++ omocha-api/src/main/java/org/omocha/api/ApiServerApplication.java
@@ -2,8 +2,10 @@ package org.omocha.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableRetry
 @EnableScheduling
 @SpringBootApplication(
 	scanBasePackages = {"org.omocha.api", "org.omocha.domain", "org.omocha.infra"}

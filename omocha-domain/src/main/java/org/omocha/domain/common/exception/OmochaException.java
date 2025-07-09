f1329756800806874687694d3061a2bd15fb0908
song <shd2768@naver.com>
2025-07-09 21:00:46 +0900

    refactor: 최상위 Custom Exception 수정

--- omocha-domain/src/main/java/org/omocha/domain/common/exception/OmochaException.java
+++ omocha-domain/src/main/java/org/omocha/domain/common/exception/OmochaException.java
@@ -7,17 +7,14 @@ import lombok.Getter;
 @Getter
 public abstract class OmochaException extends RuntimeException {
 	private final ErrorCode errorCode;
-	private final String message;
 
 	public OmochaException(ErrorCode errorCode, String message) {
 		super(message);
 		this.errorCode = errorCode;
-		this.message = message;
 	}
 
 	public OmochaException(ErrorCode errorCode, String message, Exception cause) {
 		super(message, cause);
 		this.errorCode = errorCode;
-		this.message = message;
 	}
 }
